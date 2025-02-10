f07063fc7019c4ba2e858a56f9ffa3a187f10df3
Sander Lukas <sander915@hotmail.com>
2025-02-10 23:08:36 +0200

    AnnyAI project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "anny-ai-user-service"
---
+++ src/test/java/com/annyai/user/AnnyAiUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annyai.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnyAiUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
